fc59b05c5c915d97ef614485c9b32b39d084db1b
Keaton Sant <43452799+KeatonBrazil@users.noreply.github.com>
2020-01-31 13:12:30 -0700

    You are dumb

--- src/main/java/Testing.java
+++ src/main/java/Testing.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Testing {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Your name");
+        Scanner prompt = new Scanner(System.in);
+        System.out.println("Who is dumb? ");
+        String dumb = prompt.nextLine(); 
+        System.out.println("Wrong! You are dumb!");       
+        
     }
 }
